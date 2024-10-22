87df56cf8ac932ddee3048670888ab1e12bcab20
juaneleanor <emjuan@up.edu.ph>
2024-10-23 01:36:02 +0800

    Post Function

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,25 @@ dotenv.config();
 
 const app = express();
 
-app.get("/products", (req, res) => {});
+app.post("/products", async (req, res) => {
+  const product = req.body; // User input
+
+  if (!product.name || !product.price || !product.image) {
+    return res
+      .status(400)
+      .json({ success: false, message: "Please provide all fields" });
+  }
+
+  const newProduct = new Product(product);
+
+  try {
+    await newProduct.save();
+    res.status(201).json({ success: true, data: newProduct });
+  } catch (error) {
+    console.error("Error in Create product:", error.message);
+    res.status(500).json({ success: false, message: "Server Error" });
+  }
+});
 
 app.listen(5000, () => {
   connectDB();
