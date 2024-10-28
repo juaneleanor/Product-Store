fde58e8341239e96e9e833d197ef1a230203af88
juaneleanor <emjuan@up.edu.ph>
2024-10-28 18:31:13 +0800

    Home Page Header

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,7 +1,22 @@
+import { Container, Text, VStack } from "@chakra-ui/react";
 import React from "react";
 
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <Container maxW="container.xl" py={12}>
+      <VStack spacing={8}>
+        <Text
+          fontSize={"30"}
+          fontWeight={"bold"}
+          bgGradient={"linear(to-r, cyan.400, blue.500)"}
+          bgClip={"text"}
+          textAlign={"center"}
+        >
+          Current Products 🚀
+        </Text>
+      </VStack>
+    </Container>
+  );
 };
 
 export default HomePage;
