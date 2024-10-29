5ed72e82fc4ccafe79734803cd502ee3b1314169
juaneleanor <emjuan@up.edu.ph>
2024-10-29 20:58:16 +0800

    Integrated Delete Function

---
+++ frontend/src/components/ProductCard.jsx
@@ -0,0 +1,78 @@
+import {
+  Box,
+  Heading,
+  HStack,
+  IconButton,
+  Image,
+  Text,
+  useColorModeValue,
+  useToast,
+} from "@chakra-ui/react";
+
+import { GrEdit } from "react-icons/gr";
+import { RiDeleteBin6Fill } from "react-icons/ri";
+import { useProductStore } from "../store/product";
+
+const ProductCard = ({ product }) => {
+  const textColor = useColorModeValue("gray.600", "gray.200");
+  const bg = useColorModeValue("white", "gray.800");
+
+  const { deleteProduct } = useProductStore();
+  const toast = useToast();
+  const handleDeleteProduct = async (pid) => {
+    const { success, message } = await deleteProduct(pid);
+    if (!success) {
+      toast({
+        title: "Error",
+        description: message,
+        status: "error",
+        isClosable: true,
+      });
+    } else {
+      toast({
+        title: "Success",
+        description: message,
+        status: "success",
+        isClosable: true,
+      });
+    }
+  };
+
+  return (
+    <Box
+      shadow="lg"
+      rounded="lg"
+      overflow="hidden"
+      transition={"all .3s"}
+      _hover={{ transform: "translateY(-5px)", shadow: "xl" }}
+      bg={bg}
+    >
+      <Image
+        src={product.image}
+        alt={product.name}
+        h={48}
+        w="full"
+        objectFit={"cover"}
+      />
+      <Box p={4}>
+        <Heading as="h3" size="md" mb={2}>
+          {product.name}
+        </Heading>
+        <Text fontWeight={"bold"} fontSize={"xl"} color={textColor} mb={4}>
+          ${product.price}
+        </Text>
+
+        <HStack spacing={2}>
+          <IconButton icon={<GrEdit />} colorScheme="blue" />
+          <IconButton
+            icon={<RiDeleteBin6Fill />}
+            onClick={() => handleDeleteProduct(product._id)}
+            colorScheme="red"
+          />
+        </HStack>
+      </Box>
+    </Box>
+  );
+};
+
+export default ProductCard;
--- frontend/src/store/product.js
+++ frontend/src/store/product.js
@@ -22,4 +22,22 @@ export const useProductStore = create((set) => ({
 
     return { success: true, message: "Product created successfully." };
   },
+  fetchProducts: async () => {
+    const res = await fetch("/api/products");
+    const data = await res.json();
+    set({ products: data.data });
+  },
+  deleteProduct: async (pid) => {
+    const res = await fetch(`/api/products/${pid}`, {
+      method: "DELETE",
+    });
+    const data = await res.json();
+    if (!data.success) return { success: false, message: data.message };
+
+    //Update the UI immediately, without needing a refresh
+    set((state) => ({
+      products: state.products.filter((product) => product._id !== pid),
+    }));
+    return { success: true, message: data.message };
+  },
 }));
